0b4425f57643bd3eff5999e6ea705cd4eac1b8da
Stevan Earl <stevan.earl@asu.edu>
2017-04-10 14:58:12 -0700

    added brief readme statment, and added getdata.py script

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # local_pasta_catalog
 Local PASTA catalog
+
+This repository details example code appropriate to harvest metadata records from PASTA+, and style them for the purpose of generating a local data catalog.
---
+++ getdata.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+# query parameters 
+defType="edismax" 
+queryText="author:*" 
+scopeSearch=["scope:knb-lter-sbc","-scope:ecotrends","-scope:lter-landsat*"] 
+fieldsReturned='*' 
+sortList=["score,desc","packageid,asc"] 
+doDebug="false" 
+startRecord=1 
+returnNumRows=10
+
+queryString="defType="+defType 
+if (len(queryText) > 0): 
+    queryString +="&q="+queryText 
+for scope in scopeSearch: 
+    queryString+="&fq="+scope 
+if (len(fieldsReturned) > 0): 
+    queryString+="&fl="+fieldsReturned 
+for sort in sortList: 
+    queryString+="&sort="+sort 
+if (len(doDebug) > 0): 
+    queryString+="&debug="+doDebug 
+if (startRecord > 0): 
+    queryString+="&start="+str(startRecord) 
+if (returnNumRows > 0): 
+    queryString+="&rows="+str(returnNumRows) 
+print(queryString) 
+
+u=urllib.request.urlopen("https://pasta-d.lternet.edu/package/search/eml?"+queryString) 
+
+print(u.read().decode('utf-8'))
+
